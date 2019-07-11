7bb3467b30bddc83a61d6fb735fcfe4d4b91ae0e
Chun-Ja <52552024+Chun-Ja@users.noreply.github.com>
2019-07-11 13:24:10 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function(){
+  $(window).on("scroll",function(){
+    if($(window).scrollTop()>=800 && $(window).scrollTop()<=1600){
+      $('.chart').easyPieChart({
+        //your configuration goes here
+        size:200,
+        lineWidth:20,
+         
+      });
+    }else{
+      $(".chart").reset();
+    }
+  });
+});
